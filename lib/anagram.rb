6b80dc44452a2b148d093c5d8f301a679f28aaf6
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-19 00:55:54 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :anagram, :arr
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(arr)
+    if arr.each {|word| word} == anagram.reverse
+      return arr
+    end
+  end
+end
