8629d0aec4112746cc2690cee5325ccd06ab336f
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-19 00:54:49 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :anagram, :arr
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(arr)
+    if arr.each {|word| word == anagram}
+      return anagram
+    end 
+  end
+end
